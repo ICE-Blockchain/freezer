f04d1fcb564282363d2390cbe8c28e6e29442423
ice-cronus <105345303+ice-cronus@users.noreply.github.com>
2024-06-06 17:46:07 +0300

    ddl fix

--- tokenomics/globalDDL.sql
+++ tokenomics/globalDDL.sql
@@ -3,9 +3,9 @@
 CREATE TABLE IF NOT EXISTS mining_boost_accepted_transactions (
                                                    created_at                             TIMESTAMP NOT NULL,
                                                    mining_boost_level                     SMALLINT NOT NULL,
-                                                   tenant                                 TEXT,
-                                                   tx_hash                                TEXT UNIQUE,
-                                                   ice_amount                             TEXT,
-                                                   sender_address                         TEXT,
-                                                   user_id                                TEXT,
+                                                   tenant                                 TEXT NOT NULL,
+                                                   tx_hash                                TEXT UNIQUE NOT NULL,
+                                                   ice_amount                             TEXT NOT NULL,
+                                                   sender_address                         TEXT NOT NULL,
+                                                   user_id                                TEXT NOT NULL,
                                             primary key(user_id,tx_hash));
